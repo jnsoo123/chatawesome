319e368eb807ab29442d8b3e9b5d553e065398ac
John Neil Soo <neil.soo@adish.co.jp>
2016-09-22 09:53:33 +0800

    Added new channel function

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -4,14 +4,31 @@ class ChatroomsController < ApplicationController
     @chatrooms = Chatroom.all
   end
 
+  def new
+    @chatroom = Chatroom.new
+  end
+
   def show
     @chatroom = Chatroom.find(params[:id].to_i)
     @message = Message.new
   end
 
   def create
+    @chatroom = Chatroom.new(chatroom_params)
+    if @chatroom.save
+      redirect_to chatrooms_path
+    else
+      flash[:error] = @chatroom.errors.full_messages.join(',')
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:topic)
   end
 end
