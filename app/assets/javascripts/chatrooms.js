e1143604038ad925b916530bb41e0ff51d64d538
John Neil Soo <neil.soo@adish.co.jp>
2016-09-23 16:16:47 +0800

    Fixed Dropdown issue and added viewport for mobile viewing

--- app/assets/javascripts/chatrooms.js
+++ app/assets/javascripts/chatrooms.js
@@ -1,6 +1,5 @@
 $(document).on('turbolinks:load', function() {
-  msgBox = $('.messages')
-  msgBox.animate({
+  $('.messages').animate({
       scrollTop: $('.messages')[0].scrollHeight
   }, 500);
 });
