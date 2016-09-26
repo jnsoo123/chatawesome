6b4f6e9cae92db3b6a613774d714b1eda6ccb64d
John Neil Soo <neil.soo@adish.co.jp>
2016-09-26 16:08:13 +0800

    added dynamic password field in new chatroom

--- app/assets/javascripts/chatrooms.js
+++ app/assets/javascripts/chatrooms.js
@@ -1,5 +1,13 @@
 $(document).on('turbolinks:load', function() {
+  
+  $('#chatroom_is_private').change(function(){
+    $('#chatroom_password').attr('disabled', !this.checked)
+  });
+  $('#chatroom_password').attr('disabled', !$('#chatroom_is_private').checked)
   $('.messages').animate({
       scrollTop: $('.messages')[0].scrollHeight
   }, 500);
+  
 });
+
+
