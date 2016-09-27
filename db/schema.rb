71cc65ffd6aa895c9378f9e25a94f73f6af1f292
John Neil Soo <neil.soo@adish.co.jp>
2016-09-27 09:44:33 +0800

    added user id to chatroom for chatroom owner

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160925235704) do
+ActiveRecord::Schema.define(version: 20160927014354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20160925235704) do
     t.datetime "updated_at",                      null: false
     t.boolean  "is_private",      default: false
     t.string   "password_digest"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_chatrooms_on_user_id", using: :btree
   end
 
   create_table "messages", force: :cascade do |t|
@@ -51,6 +53,7 @@ ActiveRecord::Schema.define(version: 20160925235704) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "chatrooms", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
 end
