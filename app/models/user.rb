225e2250361f0220e133ca942d71d909de55bde2
John Neil Soo <neil.soo@adish.co.jp>
2016-09-27 09:19:13 +0800

    remove recoverability from devise and fixed some design

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :messages
   has_many :chatrooms, through: :messages
   devise :database_authenticatable, :registerable,
-         :recoverable, :trackable, :validatable
+         :trackable, :validatable
 
 
 end
