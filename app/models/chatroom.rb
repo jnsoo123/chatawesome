251fa2db943ae1539c187a97cf5336927b820629
John Neil Soo <neil.soo@adish.co.jp>
2016-11-15 12:18:55 +0800

    Fixed password issue when creating chatroom with password

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,6 +1,7 @@
 class Chatroom < ApplicationRecord
-  has_secure_password validations: false, if: :skip_password_validation
-  has_secure_password validations: true, on: :update_password
+  has_secure_password(validations: false)
+  # has_secure_password validations: true, on: :update_password
+
   has_many :messages, dependent: :destroy
   has_many :users, through: :messages
 
@@ -9,14 +10,10 @@ class Chatroom < ApplicationRecord
   validates :topic, presence: true, uniqueness: true, case_sensitive: false
   validates :password, length: { minimum: 6 }, unless: :skip_password_validation, on: :create
 
-
-  def not_private(state)
-    @not_private = true if state
-  end
-
   private
 
   def skip_password_validation
-    true if @not_private
+    puts !is_private
+    !is_private
   end
 end
