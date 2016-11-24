0c0868f2d7d338f34fb6fb30f15ef2e5b6980ca0
Soo <johnneilsoo@gmail.com>
2016-11-24 04:06:55 +0000

    fixing redis error

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,2 @@
-module ChattingApp
-   class << self
-     def redis
-       @redis ||= Redis.new(url: (ENV['REDISTOGO_URL'] || 'redis://127.0.0.1:6379'))
-     end
-   end
- end
+uri = URI.parse(ENV['REDISTOGO_URL' || "redis://localhost:6379/"])
+$redis = Redis.new(url: ENV['REDISTOGO_URL'])
