7e13d8c3c2ed697246925801e962444f1c52f3cd
Soo <johnneilsoo@gmail.com>
2016-11-24 04:38:30 +0000

    fixing redis error

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-uri = URI.parse("redis://redistogo:7f86431246ef65f8577b2d184990a854@crestfish.redistogo.com:10552/")
+uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
