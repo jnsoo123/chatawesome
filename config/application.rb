b71507e36de25eb79639262a956937460697ada3
Soo <johnneilsoo@gmail.com>
2016-11-24 04:53:03 +0000

    fixing redis errors

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module ChatApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    uri = URI.parse(ENV.fetch("REDISTOGO_URL", "redis://localhost:6379/"))
+    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   end
 end
