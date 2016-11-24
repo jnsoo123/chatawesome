355701fcdc9ae1ec40cad07e6a53d3616e55c7c8
Soo <johnneilsoo@gmail.com>
2016-11-24 07:46:57 +0000

    minor UI tweaks

--- app/assets/javascripts/channels/chatgroups.js
+++ app/assets/javascripts/channels/chatgroups.js
@@ -4,6 +4,6 @@ App.messages = App.cable.subscriptions.create('ChatroomsChannel', {
   },
 
   renderChannels: function(data) {
-    return "<div class='col-xs-12' style='margin-bottom: 5px;'><a href='/chatrooms/"+data.id+"' class='btn btn-primary' style='width: 100%;'><div class='clearfix'><span class='pull-left'>#"+data.channel+" ( "+data.user_count+" <i class='fa fa-user'></i>) <span class='label label-danger'>New</span></span><span class='pull-right'>"+(data.lock ? "<i class='fa fa-lock'></i>" : "")+"</span></div></a></div>";
+    return "<div class='col-xs-12' style='margin-bottom: 5px;'><a href='/chatrooms/"+data.id+"' class='btn btn-default' style='width: 100%;'><div class='clearfix'><span class='pull-left'>#"+data.channel+" ( "+data.user_count+" <i class='fa fa-user'></i> ) <span class='label label-danger'>New</span></span><span class='pull-right'>"+(data.lock ? "<i class='fa fa-lock'></i>" : "")+"</span></div></a></div>";
   }
 });
