177bb119d719545ae44e60fc2ad30f002fee3e55
John Neil Soo <neil.soo@adish.co.jp>
2016-12-22 12:04:59 +0800

    fixed message streaming bug

--- app/controllers/private_chats_controller.rb
+++ app/controllers/private_chats_controller.rb
@@ -6,6 +6,8 @@ class PrivateChatsController < ApplicationController
       @chatroom = @chatroom.where(user_two: @recipient).first
     elsif @chatroom = PrivateChat.where(user_two: current_user).presence
       @chatroom = @chatroom.where(user_one: @recipient).first
+    else 
+      @chatroom = PrivateChat.create(user_one: current_user, user_two: @recipient)
     end
     
     @message = Message.new
